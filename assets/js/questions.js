f29e065db83ac1345260ca9bd4c06b91104c3f47
Darvid-223 <david.arvidsson@hotmail.com>
2024-04-23 19:39:05 +0200

    added api request to get 10 random questions from open trivia

---
+++ assets/js/questions.js
@@ -0,0 +1,23 @@
+let fetchTriviaQuestions = async () => {
+  let url = 'https://opentdb.com/api.php?amount=10&type=multiple'; // get 10 multiple choice questions
+  let response = await fetch(url);
+  if (!response.ok) { // Check if the response was successful
+      console.error("Failed to fetch questions:", response.status);
+      return []; // Return an empty array in case of failure
+  }
+  let data = await response.json();
+  let formattedQuestions = data.results.map(item => ({
+      question: item.question,
+      options: [...item.incorrect_answers, item.correct_answer].sort(() => Math.random() - 0.5), // randomize options
+      answer: item.correct_answer
+  }));
+  return formattedQuestions;
+};
+
+// Store questions
+let getQuestions = async () => {
+  let easyQuestions = await fetchTriviaQuestions();
+  console.log(easyQuestions); // print questions
+};
+
+getQuestions();
