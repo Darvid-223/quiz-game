ed3d1bd2626120125167b91b5ed4ea3343406dc9
Darvid-223 <david.arvidsson@hotmail.com>
2024-04-27 09:53:52 +0200

    replaced temporary element to instead using he library to decode HTML

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,6 +23,41 @@ function selectAnswer(buttonId) {
 // Function to display the question
 function displayQuestion() {
   if (questions.length > 0 && currentQuestionIndex < questions.length) {
+    setTimeout(() => {
+      const currentQuestion = questions[currentQuestionIndex];
+      // Decode HTML entities in the question text
+      document.getElementById('question').textContent = he.decode(currentQuestion.question);
+      currentQuestion.options.forEach((option, index) => {
+        // Decode HTML entities in each option and update the button text
+        document.getElementById(`choice${index + 1}`).textContent = he.decode(option);
+      });
+    }, 100); // Delay to allow DOM time to update
+  } else {
+    console.log("No questions available or an error occurred"); // print a message to console if no questions are availale or other errors occurred
+  }
+}
+
+
+
+
+// Function to initiate the game
+function startNewGame() {
+  fetchTriviaQuestions().then(fetchedQuestions => {
+    questions = fetchedQuestions; // Store fetched questions
+    currentQuestionIndex = 0; // Reset the question index to start from the first question
+    displayQuestion(); // Display the first question
+  });
+}
+
+
+
+/* 
+-This code is for decoding/unescaping html without using 'he' library
+
+
+// Function to display the question
+function displayQuestion() {
+  if (questions.length > 0 && currentQuestionIndex < questions.length) {
       const currentQuestion = questions[currentQuestionIndex];
       
       const questionText = unescapeHtml(currentQuestion.question); // Unescape HTML enitites in quesion
@@ -40,20 +75,10 @@ function displayQuestion() {
 
 
 
-// Function to initiate the game
-function startNewGame() {
-  fetchTriviaQuestions().then(fetchedQuestions => {
-    questions = fetchedQuestions; // Store fetched questions
-    currentQuestionIndex = 0; // Reset the question index to start from the first question
-    displayQuestion(); // Display the first question
-  });
-}
-
-
-
 // function to unescape html entities
 function unescapeHtml(html) {
   let temp = document.createElement("div"); // Create a temporary div element
   temp.innerHTML = html; // Set the HTML content to argument
   return temp.textContent || temp.innerText || ""; // Retrieve the text from the div
 }
+*/
