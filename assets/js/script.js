0346315957e679edd7561a2c83528aff01a65975
Darvid-223 <david.arvidsson@hotmail.com>
2024-04-27 15:06:31 +0200

    current question number shows in score panel

--- assets/js/script.js
+++ assets/js/script.js
@@ -101,11 +101,17 @@ function displayQuestion() {
     setTimeout(() => {
       const currentQuestion = questions[currentQuestionIndex];
       
-      document.getElementById('question').textContent = he.decode(currentQuestion.question); // Decode HTML entities in the question text
+      document.getElementById('question').textContent = he.decode(currentQuestion.question); // update displayed question and decode HTML entities in the question text
+
       currentQuestion.options.forEach((option, index) => {
-        
-        document.getElementById(`choice${index + 1}`).textContent = he.decode(option); // Decode HTML entities in each option and update the button text
+        document.getElementById(`choice${index + 1}`).textContent = he.decode(option); // Update choice button text and decode HTML entities in each option and update the button text
       });
+
+      
+      const questionCountDisplay = document.getElementById('correctCount'); // Update the question counter on the UI
+      questionCountDisplay.textContent = `${currentQuestionIndex + 1} / ${questions.length}`; // Display the current question index + 1 since index is 0-based
+
+
     }, 100); // Delay to allow DOM time to update
   } else {
     console.log("No questions available or an error occurred"); // print a message to console if no questions are availale or other errors occurred
