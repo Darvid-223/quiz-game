03f95a4840e0dd02e99a4f48728f35ccaa0e4791
Darvid-223 <david.arvidsson@hotmail.com>
2024-04-28 17:38:34 +0200

    added new element for final score and put it over difficulty selection

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,10 +1,12 @@
-
 let fetchTriviaQuestions = async (difficulty = 'easy') => {
-  let url = `https://opentdb.com/api.php?amount=10&type=multiple&difficulty=${difficulty}`; // get 10 multiple choice question, medium as default argument
+  // get 10 multiple choice question, medium as default argument
+  let url = `https://opentdb.com/api.php?amount=10&type=multiple&difficulty=${difficulty}`; 
   let response = await fetch(url);
-  if (!response.ok) { // Check if the response was successful
+  // Check if the response was successful
+  if (!response.ok) { 
       console.error("Failed to fetch questions:", response.status);
-      return []; // Return an empty array in case of failure
+      // Return an empty array in case of failure
+      return []; 
   }
   let data = await response.json();
   let formattedQuestions = data.results.map(item => ({
@@ -14,5 +16,3 @@ let fetchTriviaQuestions = async (difficulty = 'easy') => {
   }));
   return formattedQuestions;
 };
-
-
